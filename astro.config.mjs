38a377d921498cf8dc5c2e4f916c592b9b31afd8
wing-0902 <tsubasa@me.wing.osaka>
2025-08-13 07:29:57 +0900

    Cloudflareアダプタを削除した

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import react from "@astrojs/react";
 export default defineConfig({
   site: 'https://diaries.wing.osaka',
   output: 'static',
-  adapter: cloudflare(),
   integrations: [
     sitemap(),
     react(),
