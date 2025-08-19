7c22f8c7a5cf535d1a5e7872e1fc4cd0f5a6c434
wing-0902 <wing@me.wing.osaka>
2025-08-19 22:10:23 +0000

    どうや？

---
+++ src/pages/search.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { onMount } from 'svelte';
+  import { writable } from 'svelte/store';
+
+  export const queryParam = writable('');
+
+  onMount(() => {
+    const params = new URLSearchParams(window.location.search);
+    const value = params.get('q');
+    if (value) {
+      queryParam.set(value);
+    }
+  });
+</script>
+
+<section>
+  <h2>検索中の語句： {$queryParam}</h2>
+</section>
