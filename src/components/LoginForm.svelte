0f3bbfa3d8fd9883844df4b53b445f67f78d305d
wing-0902 <wing@me.wing.osaka>
2025-08-23 04:26:25 +0000

    ログインページを作成（コメント欄用，Firebase）

---
+++ src/components/LoginForm.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+
+</script>
+
+<section>
+  <form>
+    <label for="email">メール</label><br />
+    <input placeholder="user@wing.osaka" type="email" required /><br /><br />
+    <label for="password">パスワード</label><br />
+    <input placeholder="Passw0rd" type="password" required minlength="6" /><br /><br />
+    <button type="submit">ログイン</button><br /><br />
+    <a href='/login/new'>新規登録</a>
+  </form>
+</section>
+
+<style lang='scss'>
+  section {
+    display: flex;
+    justify-content: center;
+  }
+
+  form {
+    display: block;
+    width: 100%;
+    max-width: 400px;
+  }
+
+  input {
+    width: 100%;
+  }
+
+  button, a {
+    
+  }
+</style>
---
+++ src/firebase/client.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC8wCC2sET6sL67lsBqhdnUlVYFgEIYI54",
+  authDomain: "wing-diaries-comment-80bb4.firebaseapp.com",
+  projectId: "wing-diaries-comment-80bb4",
+  storageBucket: "wing-diaries-comment-80bb4.firebasestorage.app",
+  messagingSenderId: "334467194484",
+  appId: "1:334467194484:web:a3d87266e43bfdadfefb21",
+  measurementId: "G-DGVDJXX72L"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
