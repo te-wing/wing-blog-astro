90a147b3b5831b4c043ed63ec95b0793b501ec2d
wing-0902 <wing@me.wing.osaka>
2025-08-23 07:45:15 +0000

    最高！！！！！！

--- src/components/Dash.svelte
+++ src/components/Dash.svelte
@@ -1,25 +1,11 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import { initializeApp } from 'firebase/app';
-  import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
+  import { auth } from '../firebase/client';
+  import { onAuthStateChanged, signOut } from 'firebase/auth';
 
   let user: any = null;
-  let auth: any;
 
   onMount(() => {
-    const firebaseConfig = {
-      apiKey: "AIzaSyC8wCC2sET6sL67lsBqhdnUlVYFgEIYI54",
-      authDomain: "wing-diaries-comment-80bb4.firebaseapp.com",
-      projectId: "wing-diaries-comment-80bb4",
-      storageBucket: "wing-diaries-comment-80bb4.firebasestorage.app",
-      messagingSenderId: "334467194484",
-      appId: "1:334467194484:web:a3d87266e43bfdadfefb21",
-      measurementId: "G-DGVDJXX72L"
-    };
-
-    const app = initializeApp(firebaseConfig);
-    auth = getAuth(app);
-
     const unsubscribe = onAuthStateChanged(auth, (authUser) => {
       user = authUser;
       if (!user) {
