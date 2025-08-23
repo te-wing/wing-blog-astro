efd94f4dba92a4b16ae9ae243a12797d7bfdeef2
wing-0902 <wing@me.wing.osaka>
2025-08-23 11:23:50 +0000

    関数はできた

--- src/components/ChangeDisplayName.svelte
+++ src/components/ChangeDisplayName.svelte
@@ -6,6 +6,7 @@
 
   let user: any = null;
   let nickname: string = '';
+  let errorMessage: string = '';
 
   onMount(() => {
     const unsubscribe = onAuthStateChanged(auth, (authUser) => {
@@ -25,7 +26,8 @@
         await updateProfile(user, {displayName: nickname});
       } catch (error: any) {
         errorMessage = error.message;
-        alert(`エラー：${errorMessage}`)
+        alert(`エラー：${errorMessage}`);
+        console.error(error);
       }
     }
   };
@@ -36,6 +38,9 @@
 <div>
   <form>
     <input type='text' placeholder='ニックネーム' bind:value={nickname} required/>
+    {#if errorMessage}
+      <p>{errorMessage}</p>
+    {/if}
     <button type='submit'>変更</button>
   </form>
 </div>
